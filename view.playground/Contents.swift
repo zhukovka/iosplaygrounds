14e7ba571872375b5f90388c20170e252deaa24c
zhukovka <helen.zhukova@gmail.com>
2017-11-09 22:40:05 +0200

    constraints

--- view.playground/Contents.swift
+++ view.playground/Contents.swift
@@ -4,6 +4,8 @@ import UIKit
 import PlaygroundSupport
 
 class OfflineMessageView: UIView {
+    var message:UILabel = UILabel()
+    
     required init?(coder:NSCoder) {
         super.init(coder:coder)
         setupView()
@@ -14,22 +16,45 @@ class OfflineMessageView: UIView {
     }
     func setupView() {
         //add subviews here
+        message.text = "You are offline"
+        message.textColor = .white
+        message.textAlignment = .center
+        message.backgroundColor = .black
+        addSubview(message)
+        setNeedsUpdateConstraints()
+    }
+    
+    override func updateConstraints() {
+        message.translatesAutoresizingMaskIntoConstraints = false
+        message.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        message.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        message.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        message.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        super.updateConstraints()
     }
 }
 
 class MyViewController : UIViewController {
+    var offline: UIView?
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
+        offline = OfflineMessageView()
+        offline?.backgroundColor = .black
+        view.addSubview(offline!)
 
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
         self.view = view
     }
+    
+    override func updateViewConstraints() {
+        offline?.translatesAutoresizingMaskIntoConstraints = false
+        offline?.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        offline?.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        offline?.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        super.updateViewConstraints()
+    }
+    
+    
 }
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = MyViewController()
